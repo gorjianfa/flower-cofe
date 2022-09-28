0d1b9679ff52883bbfb3e3aa2c9bd42d13ff9195
gorjianfa <grjyanfatmh3@gmail.com>
2022-09-28 17:03:27 +0330

    flower cafe project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,72 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "360px",
+      sm: "600px",
+      tablet: "640px",
+      md: "905px",
+      laptop: "1024px",
+      lg: "1240px",
+      desktop: "1280px",
+      xl: "1440px",
+    },
+    extend: {
+      fontfamily: {
+        sans: [
+          "IRANSans",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
   },
-  plugins: [],
-}
+  prefix: "tw-",
+  plugins: [
+    plugin(function ({ addComponents, addVariant }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          margin: "0 16px",
+          width: "inherit",
+
+          "@screen sm": {
+            maxWidth: "100%",
+            margin: "0 32px",
+            width: "inherit",
+          },
+          "@screen md": {
+            maxWidth: "840px",
+            margin: "0 auto",
+            width: "100%",
+          },
+          "@screen lg": {
+            maxWidth: "100%",
+            margin: "0 200px",
+            width: "inherit",
+          },
+          "@screen xl": {
+            maxWidth: "1280px",
+            margin: "0 auto",
+            width: "100%",
+          },
+        },
+      });
+      addVariant("not-last", "&:not(:last-child)");
+    }),
+  ],
+};
